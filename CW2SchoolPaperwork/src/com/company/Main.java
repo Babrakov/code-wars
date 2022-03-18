d8a554a1090458e3a4b923902c54057a0a8f3880
Sergei Babrakov <s.a.babrakov@gmail.com>
2022-03-18 10:19:03 +0300

    first commit

---
+++ CW2SchoolPaperwork/src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println( paperWork(-5,5));
+        System.out.println( paperWork(5,5));
+    }
+
+    public static int myPaperWork(int n, int m)
+    {
+        if (n<0 || m<0) {
+            return 0;
+        } else {
+            return m*n;
+        }
+    }
+
+    public static int paperWork(int n, int m)
+    {
+        return (n < 0) || (m < 0) ? 0 : n * m;
+        //Happy Coding! ^_^
+    }
+}
---
+++ CW3HowGoodAreYouReally/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(betterThanAverage(new int[] {100, 90}, 11));
+    }
+
+    public static boolean myBetterThanAverage(int[] classPoints, int yourPoints) {
+        var sum = 0;
+        for (int i = 0; i < classPoints.length; i++) {
+            sum += classPoints[i];
+        }
+        return (sum/classPoints.length)<yourPoints;
+    }
+
+    static boolean betterThanAverage(final int[] classPoints, final int yourPoints) {
+        return Arrays.stream(classPoints).average().orElse(0) < yourPoints;
+    }
+}
