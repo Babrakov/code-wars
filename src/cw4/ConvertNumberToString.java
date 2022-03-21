29909f70e820bdd8c5c1ee205e0a229e74992a16
Sergei Babrakov <s.a.babrakov@gmail.com>
2022-03-21 09:36:31 +0300

    Added cw4.

---
+++ src/cw4/ConvertNumberToString.java
@@ -0,0 +1,12 @@
+package cw4;
+
+public class ConvertNumberToString {
+
+    public static void main(String[] args) {
+        System.out.println(numberToString(67));
+    }
+
+    public static String numberToString(int num) {
+        return String.valueOf(num);
+    }
+}
