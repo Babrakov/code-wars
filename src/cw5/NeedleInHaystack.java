daa9ecd3a882eef2e69b68043e288a58174718dc
Sergei Babrakov <s.a.babrakov@gmail.com>
2022-03-21 09:49:49 +0300

    Added cw5.

---
+++ src/cw5/NeedleInHaystack.java
@@ -0,0 +1,21 @@
+package cw5;
+
+import java.util.Arrays;
+
+public class NeedleInHaystack {
+
+    public static void main(String[] args) {
+        System.out.println(findNeedle(new Object[]{"3", "123124234", null, "needle", "world", "hay", 2, "3", true, false}));
+    }
+
+    public static String myFindNeedle(Object[] haystack) {
+        for (int i=0;i<haystack.length;i++) {
+            if (haystack[i]=="needle") return "found the needle at position "+i;
+        }
+        return "";
+    }
+
+    public static String findNeedle(Object[] haystack) {
+        return String.format("found the needle at position %d", java.util.Arrays.asList(haystack).indexOf("needle"));
+    }
+}
