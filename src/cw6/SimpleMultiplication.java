8873ac483d8507cdc5c4697501bf6ebbe11b4f2e
Sergei Babrakov <s.a.babrakov@gmail.com>
2022-03-22 08:50:40 +0300

    Added even/odd check and invert values.

---
+++ src/cw6/SimpleMultiplication.java
@@ -0,0 +1,20 @@
+package cw6;
+
+public class SimpleMultiplication {
+
+    public static void main(String[] args) {
+        System.out.println(simpleMultiplication(1));
+        System.out.println(simpleMultiplication(2));
+        System.out.println(simpleMultiplication(3));
+        System.out.println(simpleMultiplication(21));
+        System.out.println(simpleMultiplication(23));
+    }
+
+    public static int mySimpleMultiplication(int n) {
+        return ((n ^ 1) == n + 1) ? n * 8 : n * 9;
+    }
+
+    public static int simpleMultiplication (int n) {
+        return n % 2 == 0 ? n * 8 : n * 9;
+    }
+}
---
+++ src/cw7/InvertValues.java
@@ -0,0 +1,23 @@
+package cw7;
+
+import java.util.Arrays;
+
+public class InvertValues {
+
+    public static void main(String[] args) {
+        int[] arr = new int[] {1,2,3,4,5};
+        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(invert(new int[] {1,2,3,4,5})));
+    }
+
+    public static int[] myInvert(int[] array) {
+        for (int i=0; i<array.length; i++) {
+            array[i] *= -1;
+        }
+        return array;
+    }
+
+    public static int[] invert(int[] array) {
+        return java.util.Arrays.stream(array).map(i -> -i).toArray();
+    }
+}
