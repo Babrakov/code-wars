b6fce92358f1519c137844f17f6fecb795710e16
Sergei Babrakov <s.a.babrakov@gmail.com>
2022-03-23 08:50:16 +0300

    Added first task.

---
+++ src/cw01/SumArrays.java
@@ -0,0 +1,16 @@
+package cw01;
+
+public class SumArrays {
+
+    public static void main(String[] args) {
+        System.out.println(sum(new double[]{1,3,4.4}));
+    }
+
+    public static double sum(double[] numbers) {
+        double result = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            result += numbers[i];
+        }
+        return result;
+    }
+}
