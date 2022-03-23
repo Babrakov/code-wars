f21d4522e95a07162d4a67c46b6d40b58462e40d
Sergei Babrakov <s.a.babrakov@gmail.com>
2022-03-23 08:45:44 +0300

    Odd/even again.

---
+++ src/cw09/OppositesAttract.java
@@ -0,0 +1,16 @@
+package cw09;
+
+public class OppositesAttract {
+
+    public static void main(String[] args) {
+        System.out.println(isLove(2,2));
+    }
+
+    public static boolean myIsLove(final int flower1, final int flower2) {
+        return (flower1+flower2)%2 > 0;
+    }
+
+    public static boolean isLove(final int flower1, final int flower2) {
+        return flower1 % 2 != flower2 % 2;
+    }
+}
