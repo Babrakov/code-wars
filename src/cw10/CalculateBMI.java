4b27e6c315dc0a72206c3e247ae5d4d63efb3e7c
Sergei Babrakov <s.a.babrakov@gmail.com>
2022-03-23 08:46:25 +0300

    Calculate BMI (body mass index)

---
+++ src/cw10/CalculateBMI.java
@@ -0,0 +1,22 @@
+package cw10;
+
+public class CalculateBMI {
+
+    public static void main(String[] args) {
+        System.out.println(bmi(80, 1.80));
+    }
+
+    public static String myBmi(double weight, double height) {
+        double index = weight / (height * height);
+        if (index<= 18.5) return "Underweight";
+        else if (index <= 25.0) return "Normal";
+        else if (index <= 30.0) return "Overweight";
+        return "Obese";
+    }
+
+    public static String bmi(double weight, double height) {
+        double bmi =weight/(height*height);
+        return bmi <= 18.5 ? "Underweight": bmi <=25.0 ? "Normal" : bmi<=30.0 ? "Overweight" : "Obese";
+    }
+
+}
