72b055ef013c4ba6a30b55febece38947172ff61
Sergei Babrakov <s.a.babrakov@gmail.com>
2022-03-23 08:45:26 +0300

    Convert boolean to string.

---
+++ src/cw08/ConvertBooleanValuesToStrings.java
@@ -0,0 +1,14 @@
+package cw08;
+
+public class ConvertBooleanValuesToStrings {
+
+    public static void main(String[] args) {
+        System.out.println(boolToWord(true));
+    }
+
+    public static String boolToWord(boolean b)
+    {
+        return b ? "Yes" : "No";
+    }
+
+}
