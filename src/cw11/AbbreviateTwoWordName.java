2e7bd4b738f0396091ebd97231da3ef357e4c8f0
Sergei Babrakov <s.a.babrakov@gmail.com>
2022-03-24 09:18:19 +0300

    Abbreviate words.

---
+++ src/cw11/AbbreviateTwoWordName.java
@@ -0,0 +1,18 @@
+package cw11;
+
+public class AbbreviateTwoWordName {
+
+    public static void main(String[] args) {
+        System.out.println(abbrevName("Sam Harris"));
+    }
+
+    public static String myAbbrevName(String name) {
+        String[] splited = name.split("\\s+");
+        return splited[0].substring(0,1).toUpperCase()+"."+splited[1].substring(0,1).toUpperCase();
+    }
+
+    public static String abbrevName(String name) {
+        String[] names = name.split(" ");
+        return (names[0].charAt(0) + "." + names[1].charAt(0)).toUpperCase();
+    }
+}
