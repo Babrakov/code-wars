02848744d44f6107d9141d3e412a54c81150dca7
Sergei Babrakov <s.a.babrakov@gmail.com>
2022-03-26 12:03:26 +0300

    Mumbling the strings.

---
+++ src/main/java/cw15/Mumbling.java
@@ -0,0 +1,39 @@
+package main.java.cw15;
+
+public class Mumbling {
+
+    public static void main(String[] args) {
+        System.out.println(accum("ZpglnRxqenU"));
+    }
+
+    public static String myAccum(String s) {
+        String res = "";
+        char[] arr = s.toLowerCase().toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            if (res != "")
+                res += "-";
+            String first = "";
+            String current = "";
+            first += arr[i];
+            current = first.toUpperCase();
+            for (int j = 0; j < i; j++) {
+                current += arr[i];
+            }
+            res += current;
+        }
+        return res;
+    }
+
+    public static String accum(String s) {
+        StringBuilder bldr = new StringBuilder();
+        int i = 0;
+        for(char c : s.toCharArray()) {
+            if(i > 0) bldr.append('-');
+            bldr.append(Character.toUpperCase(c));
+            for(int j = 0; j < i; j++) bldr.append(Character.toLowerCase(c));
+            i++;
+        }
+        return bldr.toString();
+    }
+
+}
