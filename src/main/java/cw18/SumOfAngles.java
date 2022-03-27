f5d217d9482049870cab6a8b85dea565e70fcd63
Sergei Babrakov <s.a.babrakov@gmail.com>
2022-03-27 11:03:57 +0300

    Added solution for sum of angles.

--- src/main/java/cw18/SumOfAngles.java
+++ src/main/java/cw18/SumOfAngles.java
@@ -1,2 +1,8 @@
-package main.java.cw18;public class SumOfAngles {
+package main.java.cw18;
+
+public class SumOfAngles {
+
+    public static int sumOfAngles(int n) {
+        return 180 * (n - 2);
+    }
 }
--- src/test/java/cw18/AnglesSumTest.java
+++ src/test/java/cw18/AnglesSumTest.java
@@ -1,2 +1,14 @@
-package test.java.cw18;public class AnglesSumTest {
+package test.java.cw18;
+
+import main.java.cw18.SumOfAngles;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AnglesSumTest {
+    @Test
+    public void sampleTests() {
+        assertEquals(180, SumOfAngles.sumOfAngles(3));
+        assertEquals(360, SumOfAngles.sumOfAngles(4));
+    }
 }
