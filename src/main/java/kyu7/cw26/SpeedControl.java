77314827b1f855041134a6f0b3b50e81d8b1f996
Sergei Babrakov <s.a.babrakov@gmail.com>
2022-03-30 12:49:26 +0300

    Get maximum speed from GPS tracker.

---
+++ src/main/java/kyu7/cw26/SpeedControl.java
@@ -0,0 +1,26 @@
+package main.java.kyu7.cw26;
+
+import java.util.stream.IntStream;
+
+public class SpeedControl {
+
+    public static int myGps(int s, double[] x) {
+        if (x.length<=1) return 0;
+        double max = 0;
+        for (int i = 0; i < x.length-1; i++) {
+            double current;
+            current = (x[i+1]-x[i])*3600/s;
+            max = (max>current) ? max : current;
+        }
+        return (int) max;
+    }
+
+    public static int gps(int s, double[] x) {
+        double maxSpeed = IntStream
+                .range(0, x.length - 1)
+                .mapToDouble(i -> (x[i+1] - x[i]) * 3600.0 / (double) s )
+                .max().orElse(0.0);
+        return (int) Math.floor(maxSpeed);
+    }
+
+}
