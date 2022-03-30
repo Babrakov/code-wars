6ca45b145682fbc4367245e525778a1141420837
Sergei Babrakov <s.a.babrakov@gmail.com>
2022-03-30 12:48:13 +0300

    Two strings to one.

---
+++ src/main/java/kyu7/cw23/TwoToOne.java
@@ -0,0 +1,17 @@
+package main.java.kyu7.cw23;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class TwoToOne {
+
+    public static String myLongest (String s1, String s2) {
+        String str = s1+s2;
+        return str.chars().distinct().sorted().mapToObj(c -> String.valueOf((char) c)).collect(Collectors.joining());
+    }
+
+    public static String longest (String s1, String s2) {
+        String s = s1 + s2;
+        return s.chars().distinct().sorted().collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append).toString();
+    }
+}
---
+++ src/test/java/kyu7/cw23/TwoToOneTest.java
@@ -0,0 +1,16 @@
+package test.java.kyu7.cw23;
+
+import main.java.kyu7.cw23.TwoToOne;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TwoToOneTest {
+    @Test
+    public void test() {
+        System.out.println("longest Fixed Tests");
+        assertEquals("aehrsty", TwoToOne.longest("aretheyhere", "yestheyarehere"));
+        assertEquals("abcdefghilnoprstu", TwoToOne.longest("loopingisfunbutdangerous", "lessdangerousthancoding"));
+        assertEquals("acefghilmnoprstuy", TwoToOne.longest("inmanylanguages", "theresapairoffunctions"));
+    }
+}
