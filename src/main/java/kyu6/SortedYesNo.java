652c9b22891561e639daddfce962080cab9415d7
Sergei Babrakov <s.a.babrakov@gmail.com>
2022-03-31 08:44:08 +0300

    Check if array is sorted and how.

---
+++ src/main/java/kyu6/SortedYesNo.java
@@ -0,0 +1,28 @@
+package main.java.kyu6;
+
+import java.util.stream.IntStream;
+
+public class SortedYesNo {
+
+    public static String myIsSortedAndHow(int[] array) {
+        boolean asc = true, desc = true;
+        int last = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (asc && array[i]<last) asc = false;
+            if (desc && array[i]>last) desc = false;
+            last = array[i];
+        }
+        return (asc) ? "yes, ascending" : (desc) ? "yes, descending" : "no";
+    }
+
+    public static String isSortedAndHow(int[] array) {
+        if(IntStream.range(0, array.length-1).allMatch(i -> array[i] <= array[i+1]))
+            return "yes, ascending";
+
+        if(IntStream.range(0, array.length-1).allMatch(i -> array[i] >= array[i+1]))
+            return "yes, descending";
+
+        return "no";
+    }
+
+}
---
+++ src/test/java/kyu6/SortedYesNoTest.java
@@ -0,0 +1,28 @@
+package test.java.kyu6;
+
+import main.java.kyu6.SortedYesNo;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SortedYesNoTest {
+
+    @Test
+    public void test1() {
+        int[] array = new int[] {1, 2};
+        assertEquals("yes, ascending", SortedYesNo.isSortedAndHow(array));
+    }
+
+    @Test
+    public void test2() {
+        int[] array = new int[] {15, 7, 3, -8};
+        assertEquals("yes, descending", SortedYesNo.isSortedAndHow(array));
+    }
+
+    @Test
+    public void test3() {
+        int[] array = new int[] {4, 2, 30};
+        assertEquals("no", SortedYesNo.isSortedAndHow(array));
+    }
+
+}
