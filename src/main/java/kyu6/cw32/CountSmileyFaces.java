4c50e32e872808785198ac1c2b80a3cc8dde6799
Sergei Babrakov <s.a.babrakov@gmail.com>
2022-04-03 13:59:07 +0300

    Added descrition.

--- src/main/java/kyu6/cw32/CountSmileyFaces.java
+++ src/main/java/kyu6/cw32/CountSmileyFaces.java
@@ -3,6 +3,27 @@ package main.java.kyu6.cw32;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+Given an array (arr) as an argument complete the function countSmileys that should return the total number of smiling faces.
+
+Rules for a smiling face:
+
+Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ;
+A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
+Every smiling face must have a smiling mouth that should be marked with either ) or D
+No additional characters are allowed except for those mentioned.
+
+Valid smiley face examples: :) :D ;-D :~)
+Invalid smiley faces: ;( :> :} :]
+
+Example
+countSmileys([':)', ';(', ';}', ':-D']);       // should return 2;
+countSmileys([';D', ':-(', ':-)', ';~)']);     // should return 3;
+countSmileys([';]', ':[', ';*', ':$', ';-D']); // should return 1;
+Note
+In case of an empty array return 0. You will not be tested with invalid input (input will always be an array). Order of the face (eyes, nose, mouth) elements will always be the same.
+ */
+
 public class CountSmileyFaces {
 
     public static void main(String[] args) {
