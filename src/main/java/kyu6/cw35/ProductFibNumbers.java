8eca2bc9a13706dce4b1b2da08251fe0c1ac5299
Sergei Babrakov <s.a.babrakov@gmail.com>
2022-04-06 14:43:15 +0300

    Check if multiply of two Fibonacci number is a given number.

---
+++ src/main/java/kyu6/cw35/ProductFibNumbers.java
@@ -0,0 +1,85 @@
+package main.java.kyu6.cw35;
+
+/*
+The Fibonacci numbers are the numbers in the following integer sequence (Fn):
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, ...
+
+such as
+F(n) = F(n-1) + F(n-2) with F(0) = 0 and F(1) = 1.
+
+Given a number, say prod (for product), we search two Fibonacci numbers F(n) and F(n+1) verifying
+
+F(n) * F(n+1) = prod.
+
+Your function productFib takes an integer (prod) and returns an array:
+
+[F(n), F(n+1), true] or {F(n), F(n+1), 1} or (F(n), F(n+1), True)
+depending on the language if F(n) * F(n+1) = prod.
+
+If you don't find two consecutive F(n) verifying F(n) * F(n+1) = prodyou will return
+
+[F(n), F(n+1), false] or {F(n), F(n+1), 0} or (F(n), F(n+1), False)
+F(n) being the smallest one such as F(n) * F(n+1) > prod.
+
+Some Examples of Return:
+(depend on the language)
+
+productFib(714) # should return (21, 34, true),
+                # since F(8) = 21, F(9) = 34 and 714 = 21 * 34
+
+productFib(800) # should return (34, 55, false),
+                # since F(8) = 21, F(9) = 34, F(10) = 55 and 21 * 34 < 800 < 34 * 55
+-----
+productFib(714) # should return [21, 34, true],
+productFib(800) # should return [34, 55, false],
+-----
+productFib(714) # should return {21, 34, 1},
+productFib(800) # should return {34, 55, 0},
+-----
+productFib(714) # should return {21, 34, true},
+productFib(800) # should return {34, 55, false},
+ */
+
+import java.lang.Math;
+
+public class ProductFibNumbers {
+
+    public static void main(String[] args) {
+        productFib(714);
+    }
+
+    public static long[] myProductFib(long prod) {
+        long first=0,last=1,result=0,sum;
+
+        while (result<prod) {
+            sum = first+last;
+            first = last;
+            last = sum;
+            result = first*last;
+        }
+
+        if (result==prod) return new long[] {first,last,1};
+        return (result>prod) ? new long[] {first,last,0} : new long[] {last,first+last,0};
+
+    }
+
+    public static long fibonacci(long n) {
+        return Math.round((Math.pow((1+Math.sqrt(5))/2,n)-Math.pow((1-Math.sqrt(5))/2,n))/Math.sqrt(5));
+    }
+
+    public static long findN(long f) {
+        return Math.round(Math.log(f*Math.sqrt(5))/Math.log((1+Math.sqrt(5))/2));
+    }
+
+    public static long[] productFib(long prod) {
+        long a = 0L;
+        long b = 1L;
+        while (a * b < prod) {
+            long tmp = a;
+            a = b;
+            b = tmp + b;
+        }
+        return new long[] { a, b, a * b == prod ? 1 : 0 };
+    }
+
+}
---
+++ src/test/java/kyu6/cw35/ProductFibNumbersTest.java
@@ -0,0 +1,21 @@
+package test.java.kyu6.cw35;
+
+import main.java.kyu6.cw35.ProductFibNumbers;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class ProductFibNumbersTest {
+
+    @Test
+    public void test1() {
+        long[] r = new long[] {55, 89, 1};
+        assertArrayEquals(r, ProductFibNumbers.productFib(4895));
+    }
+    @Test
+    public void test2() {
+        long[] r = new long[] {89, 144, 0};
+        assertArrayEquals(r, ProductFibNumbers.productFib(5895));
+    }
+
+}
