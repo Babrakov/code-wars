d921f488d23a1f557fecc8db10800f1650ce7741
Sergei Babrakov <s.a.babrakov@gmail.com>
2022-04-08 10:51:03 +0300

    Implemented necessary methods and test.

--- src/main/java/kyu6/cw36/PaginationHelper.java
+++ src/main/java/kyu6/cw36/PaginationHelper.java
@@ -25,26 +25,30 @@ import java.util.List;
 
 public class PaginationHelper<I> {
 
+    private List<I> collection;
+    private int itemsPerPage;
+
     /**
      * The constructor takes in an array of items and a integer indicating how many
      * items fit within a single page
      */
     public PaginationHelper(List<I> collection, int itemsPerPage) {
-
+        this.collection = collection;
+        this.itemsPerPage = itemsPerPage;
     }
 
     /**
      * returns the number of items within the entire collection
      */
     public int itemCount() {
-        return 0;
+        return collection.size();
     }
 
     /**
      * returns the number of pages
      */
     public int pageCount() {
-        return 2;
+        return (int) Math.ceil((double) itemCount()/itemsPerPage);
     }
 
     /**
@@ -52,7 +56,10 @@ public class PaginationHelper<I> {
      * this method should return -1 for pageIndex values that are out of range
      */
     public int pageItemCount(int pageIndex) {
-        return 0;
+        if (pageIndex<0 || pageIndex>pageCount()-1) return -1;
+        int first = itemsPerPage*pageIndex;
+        int last = Math.min(itemsPerPage*(pageIndex+1),itemCount());
+        return last-first;
     }
 
     /**
@@ -60,7 +67,8 @@ public class PaginationHelper<I> {
      * this method should return -1 for itemIndex values that are out of range
      */
     public int pageIndex(int itemIndex) {
-        return 0;
+        if (itemIndex<0 || itemIndex>itemCount()-1) return -1;
+        return (int) Math.floor((double) itemIndex/itemsPerPage);
     }
 
 }
--- src/test/java/kyu6/cw36/PaginationHelperTest.java
+++ src/test/java/kyu6/cw36/PaginationHelperTest.java
@@ -10,9 +10,51 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class PaginationHelperTest {
 
     @Test
-    void testSomething() {
+    void testInteger() {
+        PaginationHelper<Integer> helper = new PaginationHelper(Arrays.asList(1,2,3,4,5), 2);
+        assertEquals(3, helper.pageCount());
+    }
+
+    @Test
+    void testPageCount() {
         PaginationHelper<Character> helper = new PaginationHelper(Arrays.asList('a', 'b', 'c', 'd', 'e', 'f'), 4);
         assertEquals(2, helper.pageCount());
+    }
+
+    @Test
+    void testItemsCount() {
+        PaginationHelper<Character> helper = new PaginationHelper(Arrays.asList('a', 'b', 'c', 'd', 'e', 'f'), 4);
+        assertEquals(6, helper.itemCount());
+    }
+
+    @Test
+    void testNoPageItemCount() {
+        PaginationHelper<Character> helper = new PaginationHelper(Arrays.asList('a', 'b', 'c', 'd', 'e', 'f'), 4);
+        assertEquals(-1, helper.pageItemCount(2));
+    }
+
+    @Test
+    void testPageItemCount() {
+        PaginationHelper<Character> helper = new PaginationHelper(Arrays.asList('a', 'b', 'c', 'd', 'e', 'f'), 4);
+        assertEquals(2, helper.pageItemCount(1));
+    }
+
+    @Test
+    void testBiggerPageItemCount() {
+        PaginationHelper<Character> helper = new PaginationHelper(Arrays.asList('a', 'b', 'c', 'd', 'e', 'f','g','h'), 4);
+        assertEquals(4, helper.pageItemCount(0));
+    }
+
+    @Test
+    void testNoPageIndex() {
+        PaginationHelper<Character> helper = new PaginationHelper(Arrays.asList('a', 'b', 'c', 'd', 'e', 'f'), 4);
+        assertEquals(-1, helper.pageIndex(20));
+    }
+
+    @Test
+    void testPageIndex() {
+        PaginationHelper<Character> helper = new PaginationHelper(Arrays.asList('a', 'b', 'c', 'd', 'e', 'f'), 4);
+        assertEquals(0, helper.pageIndex(2));
     }
 
 }
