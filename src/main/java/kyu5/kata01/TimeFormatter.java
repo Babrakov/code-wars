dae8ab5e77dd17e29aff57f3f820300056bfbd80
Sergei Babrakov <s.a.babrakov@gmail.com>
2022-04-12 09:02:05 +0300

    Added human readable duration format task.

---
+++ src/main/java/kyu5/kata01/TimeFormatter.java
@@ -0,0 +1,9 @@
+package main.java.kyu5.kata01;
+
+public class TimeFormatter {
+
+    public static String formatDuration(int seconds) {
+        return "";
+    }
+
+}
---
+++ src/test/java/kyu5/kata01/TimeFormatterTest.java
@@ -0,0 +1,19 @@
+package test.java.kyu5.kata01;
+
+import main.java.kyu5.kata01.TimeFormatter;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TimeFormatterTest {
+
+    @Test
+    public void exampleTests() {
+        assertEquals("1 second", TimeFormatter.formatDuration(1));
+        assertEquals("1 minute and 2 seconds", TimeFormatter.formatDuration(62));
+        assertEquals("2 minutes", TimeFormatter.formatDuration(120));
+        assertEquals("1 hour", TimeFormatter.formatDuration(3600));
+        assertEquals("1 hour, 1 minute and 2 seconds", TimeFormatter.formatDuration(3662));
+    }
+
+}
