f108bc0d42af8090fe821db4b9f00f6c611291f1
Sergei Babrakov <s.a.babrakov@gmail.com>
2022-04-13 09:46:02 +0300

    Fixed the test.

--- src/test/java/kyu7/kata10/FactorialTest.java
+++ src/test/java/kyu7/kata10/FactorialTest.java
@@ -5,7 +5,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class FactorialTest {
